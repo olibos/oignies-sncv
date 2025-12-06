bb73d470307cd5619c4c58d861b85227f5109dd5
Olivier Bossaer <olivier.bossaer@gmail.com>
2025-12-06 17:14:05 +0100

    Add PWA

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
+import { useRegisterSW } from "virtual:pwa-register/react";
 import { useModuleCommunication } from "./contexts/ModuleCommunication";
 import Control from "./Control";
 import Splash from "./Splash";
 
 export default function Application() {
     const { isConnected } = useModuleCommunication();
+    useRegisterSW();
     
     return (
         isConnected 
